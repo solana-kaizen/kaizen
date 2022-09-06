324e1c3452ddc5f4899f282dc08b51d6a3d7c202
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2022-09-06 23:00:05 +0300

    replace pubkey aggregator functions to writable and readable account metas so that aggregators can produce read/write account metas depending on usage intent (lookup/modify)

--- src/container/interfaces.rs
+++ src/container/interfaces.rs
@@ -1,5 +1,5 @@
-use solana_program::pubkey::Pubkey;
 use async_trait::async_trait;
+use solana_program::instruction::AccountMeta;
 use workflow_allocator::result::Result;
 
 
@@ -7,5 +7,6 @@ use workflow_allocator::result::Result;
 #[async_trait(?Send)]
 pub trait AccountAggregator {
     type Key;
-    async fn locate_account_pubkeys(&self,key : &Self::Key) -> Result<Vec<Pubkey>>;
+    async fn writable_account_metas(&self,key : Option<&Self::Key>) -> Result<Vec<AccountMeta>>;
+    async fn readonly_account_metas(&self,key : Option<&Self::Key>) -> Result<Vec<AccountMeta>>;
 }
