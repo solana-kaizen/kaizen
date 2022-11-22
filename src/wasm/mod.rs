31612257095e36761b96c95b9e0825aa959b11ce
Surinder Singh Matoo <surinder83singh@gmail.com>
2022-11-23 03:28:09 +0530

    cleanup

--- src/wasm/mod.rs
+++ src/wasm/mod.rs
@@ -1,17 +1,5 @@
 use wasm_bindgen::prelude::*;
-use workflow_panic_hook::{set_once, Type, show_logs as show_wasm_logs};
-#[wasm_bindgen]
-pub fn start_console_logger(){
-    set_once(Type::Console);
-}
-#[wasm_bindgen]
-pub fn start_popup_logger(){
-    set_once(Type::Popup);
-}
-#[wasm_bindgen]
-pub fn show_logs(){
-    show_wasm_logs();
-}
+pub use workflow_wasm::logs::*;
 
 pub fn bind(workflow: &JsValue, solana: &JsValue, mods: &JsValue) -> std::result::Result<(), JsValue> {
     let global = js_sys::Object::new();
