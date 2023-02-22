2167f90a77dce75927ae0911cd1c24ff4193b424
Surinder Singh Matoo <surinder83singh@gmail.com>
2023-02-23 02:08:57 +0530

    pubkey_from_value

--- src/pubkey.rs
+++ src/pubkey.rs
@@ -1,21 +1,35 @@
-use crate::result::Result;
+//use crate::result::Result;
 use solana_program::pubkey::Pubkey;
 use std::str::FromStr;
 use wasm_bindgen::prelude::*;
-use workflow_wasm::abi::ref_from_abi;
+//use workflow_wasm::abi::ref_from_abi;
+use serde_wasm_bindgen::from_value;
 
+/*
 pub trait PubkeyExt {
-    fn try_from(value: &JsValue) -> Result<Pubkey>;
+    fn try_from(value: &JsValue) -> std::result::Result<Pubkey, JsValue>;
 }
 
 impl PubkeyExt for Pubkey {
-    fn try_from(value: &JsValue) -> Result<Pubkey> {
+    fn try_from(value: &JsValue) -> std::result::Result<Pubkey, JsValue> {
         if value.is_string() {
-            Ok(Pubkey::from_str(&value.as_string().unwrap())?)
+            Ok(Pubkey::from_str(&value.as_string().unwrap())
+                .map_err(|_| JsValue::from("Invalid pubkey"))?)
         } else {
             Ok(ref_from_abi!(Pubkey, value)?)
         }
     }
+}
+*/
+
+pub fn pubkey_from_value(pubkey: JsValue) -> std::result::Result<Pubkey, JsValue> {
+    let pubkey = if let Some(pubkey_str) = pubkey.as_string() {
+        Pubkey::from_str(&pubkey_str).map_err(|_| JsValue::from("Invalid pubkey"))?
+    } else {
+        from_value(pubkey)?
+    };
+
+    Ok(pubkey)
 }
 
 /// Generates a [`Pubkey`] filled with random bytes (used explicitly in unit tests)
